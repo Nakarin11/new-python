ead3f6ac38692a3d292af865314ff03bf9505adf
Nakarin11 <NakarinChanthao36@gmail.com>
2026-07-07 15:27:17 +0700

    Lecture 2 | 07/07/2026

---
+++ ConvertInput.py
@@ -0,0 +1,6 @@
+age = input("Enter your age: ")
+age = int(age)
+
+height = input("Enter your height: ")
+height = float(height)
+print("Your age is: " + str(age) + "\nYour height is: " + str(height) + " meters.")
---
+++ testinput.py
@@ -0,0 +1,8 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello, " + first_name + " " + last_name + "! Welcome to the program.")
+
+#Input a number
+number = input("Enter a number: ")
+result = int(number) + 10
+print(result)
